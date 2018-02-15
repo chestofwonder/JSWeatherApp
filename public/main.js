7a032a60fdb161d882f8fab689e3eedf0d371154
Rubén <rubenzc@me.com>
2018-02-15 16:41:21 +0100

    Fake api call

--- public/main.js
+++ public/main.js
@@ -1,42 +1,12 @@
-/*(function(){
-
-	var httpRequest;
-
-	function makeSearch(){
-		httpRequest = new XMLHttpRequest();
-
-		if(!httpRequest){
-			alert('Cannot create an XMLHTTP instance. Sigue rascando');
-			return false;
-		}
-		httpRequest.onreadystatechange = alertContents;
-		httpRequest.open('GET', 'test.html');
-		httpRequest.send();
-
-		function alertContents(){
-
-			if (httpRequest.readyState === XMLHttpRequest.DONE){
-
-				if (httpRequest.status === 200){
-					alert(httpRequest.responseText)
-				}else{
-					alert('There was a problem with this request');
-				}
-			}
-		}//end alertContens
-	}//end makeSearch
-
-	document.getElementById("but-search").addEventListener('click', makeSearch);
-})();*/
-
 function makeSearch() {
   var xhttp = new XMLHttpRequest();
+
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (xhttp.readyState == 4) {
       document.getElementById("search").innerHTML =
-      this.responseText;
+      xhttp.responseText;
     }
   };
-  xhttp.open('GET', 'test.html');
+  xhttp.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
   xhttp.send();
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 
+
 app.listen(8080, function(){
   console.log('Server Express Ready!');
 });
