f935b03d7de2de91ed366b822bd0d3cce058b2e4
chestofwonder <chestofwonder@hotmail.com>
2018-02-21 16:21:28 +0100

    Static server
    Change the server to serve static files and not depend on express anymore

--- server.js
+++ server.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
 
-app.use(express.static('public'));
+var publicFolder = "public";
 
+http.createServer(function (req, res) {
+  var requestUrl = url.parse(req.url);
+  var pathname = './' + publicFolder + requestUrl.pathname;
 
-app.listen(8080, function(){
-  console.log('Server Express Ready!');
-});
+  const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+  };
+  
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      res.statusCode = 404;
+      res.end("File " + pathname + " not found");
+      return;
+    }
+
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/index.html';
+    }
+
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end("Error getting the file: " + err);
+      } else {
+        var ext = path.parse(pathname).ext;
+
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+}).listen(9000);
+
+console.log('Server listening on port 9000');
