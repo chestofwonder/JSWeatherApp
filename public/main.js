b5dbc994e93691a8e007a662b10851554934567b
Rubén <rubenzc@me.com>
2018-02-22 18:32:38 +0100

    Updated files

--- public/main.js
+++ public/main.js
@@ -7,6 +7,7 @@ function makeSearch() {
       xhttp.responseText;
     }
   };
-  xhttp.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
+  // xhttp.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
+  xhttp.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d479ad0f8014dde7cec0cebc52be0781', true);
   xhttp.send();
 }
